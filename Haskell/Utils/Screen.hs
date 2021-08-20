d7d243cb1f7ca4ef06044cbe73b0bc7ef8f882fc
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-08-20 09:36:05 -0300

    create another print func

--- Haskell/Utils/Screen.hs
+++ Haskell/Utils/Screen.hs
@@ -3,8 +3,13 @@ module Utils.Screen where
 import System.Info
 import System.Process
 
+putOnScreenCls :: String -> IO String
+putOnScreenCls output = do
+  if os == "linux" then system "clear" else system "cls"
+  putStr output
+  getLine
+
 putOnScreen :: String -> IO String
 putOnScreen output = do
-  -- if os == "linux" then system "clear" else system "cls"
   putStr output
   getLine
