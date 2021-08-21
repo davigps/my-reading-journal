b7202d8838dff8a672b1eb04f74c7cd7af148e8b
MatheusHG <matheus.oliveira@ccc.ufcg.edu.br>
2021-08-21 15:45:58 -0300

    create goals book screen created

--- Haskell/src/Screens/Books.hs
+++ Haskell/src/Screens/Books.hs
@@ -4,11 +4,28 @@ import Utils.Screen
 
 addBookDisplay :: IO String
 addBookDisplay = do
-  return
-    "\n=-=-=-=-=-=-=-=-=-=\nAdicionar livro\n=-=-=-=-=-=-=-=-=-=\n \
-    \a) Adicionar livro\nb) Editar livro\nc) Listar livros\nd) Excluir livro\n \
-    \Sua opção: "
+  line <- putOnScreenCls 
+          "\n=-=-=-=-=-=-=-=-=-=\nAdicionar livro\n=-=-=-=-=-=-=-=-=-=\n\
+            \Digite o nome da obra ou 'v' para voltar:"
+  
+  -- Nome da obra
+  -- Nome do autor
+  -- Data de cadastro
+  -- Gênero
+  -- Nota
+  -- Descrição 
+  if line == "v"
+    then return ""
+    else do
+      -- Falta sincronizar com a API. A ideia seria colocar tudo para lowercase e comparar com a API 
+      -- também em lowercase (ver nome e autor e gênero)
+      -- Fazer data
+      nota <- putOnScreen "Digite uma nota para a obra: "
+      descricao <- putOnScreen  "Digite uma descrição para a obra: "
 
+      putOnScreen "Seu livro foi adicionado com sucesso! (Aperte Enter para continuar)"
+      return ""
+      
 editBookDisplay :: IO String
 editBookDisplay = do
   line <-
@@ -38,7 +55,7 @@ delBookDisplay :: IO String
 delBookDisplay = do
   line <-
     putOnScreenCls
-      "\n=-=-=-=-=-=-=-=-=-=\nExcluir livro\n=-=-=-=-=-=-=-=-=-=\n \
+      "\n=-=-=-=-=-=-=-=-=-=\nExcluir livro\n=-=-=-=-=-=-=-=-=-=\n\
       \Digite o nome do livro que deseja excluir ou 'v' para voltar:"
 
   if line == "v"
@@ -46,4 +63,17 @@ delBookDisplay = do
     else do
       --deleteBook line
       putOnScreen "Seu livro foi deletado com sucesso! (Aperte Enter para continuar)"
+      return ""
+
+editBookGoalDisplay :: IO String
+editBookGoalDisplay = do
+  line <- 
+    putOnScreenCls
+      "\n=-=-=-=-=-=-=-=-=-=\nEditar meta\n=-=-=-=-=-=-=-=-=-=\n\
+      \Digite a nova meta ou 'v' para voltar:"
+  if line == "v"
+    then return ""
+    else do
+      -- atualizar a nova meta
+      putOnScreen "Sua meta foi alterada com sucesso! (Aperte Enter para continuar)"
       return ""
--- Haskell/src/Screens/Main.hs
+++ Haskell/src/Screens/Main.hs
@@ -7,17 +7,42 @@ import Utils.Screen
 
 mainMenuDisplay :: IO String
 mainMenuDisplay = do
-  line <-
-    putOnScreenCls
-      "\n=-=-=-=-=-=-=-=-=-=\nMy Reading Journal\n=-=-=-=-=-=-=-=-=-=\n\
-      \a) Adicionar livro\nb) Editar livro\nc) Listar livros\nd) Excluir livro\n\
-      \Sua opção: "
+  let currentGoal = 2
+  let currentTarget = 10
+
+  line <- putOnScreen 
+    ("\n=-=-=-=-=-=-=-=-=-=\nMy Reading Journal\n=-=-=-=-=-=-=-=-=-=\n\n" ++
+      "Meta: " ++ 
+      show currentGoal ++ 
+      "/" ++ 
+      show currentTarget ++
+      " livros\n\n" ++
+      "1) Adicionar livro\n\
+      \2) Editar livro\n\
+      \3) Listar livros\n\
+      \4) Excluir livro\n\
+      \5) Editar meta\n\n\
+      \6) Sair\n\n\n" ++
+      "Sua opção: ")
+
   mainMenuOptions line
 
 mainMenuOptions :: String -> IO String
 mainMenuOptions option
+<<<<<<< Updated upstream
   | option == "a" = addBookDisplay
   | option == "b" = editBookDisplay
   | option == "c" = seeBooksDisplay
   | option == "d" = delBookDisplay
   | otherwise = exitSuccess
+=======
+  | option == "1" = addBookDisplay
+  | option == "2" = editBookDisplay
+  | option == "3" = seeBooksDisplay
+  | option == "4" = delBookDisplay
+  | option == "5" = editBookGoalDisplay
+  | option == "6" = exitSuccess
+  | otherwise = do
+    putOnScreen "Opção invalida. Aperte enter para continuar."
+    mainMenuDisplay
+>>>>>>> Stashed changes
