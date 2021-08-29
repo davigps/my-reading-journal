dae5ed8e4748a7c4e90bf481b09952652e18ac48
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-08-29 11:39:08 -0300

    prevent empty subjects list to books api

--- Haskell/src/DataTypes/Api.hs
+++ Haskell/src/DataTypes/Api.hs
@@ -33,8 +33,10 @@ instance FromJSON BookApi where
     case s of
       Nothing ->
         return (BookApi {title = t, subject = ["Other"], author_name = a})
-      Just sub ->
-        return (BookApi {title = t, subject = sub, author_name = a})
+      Just sub -> do
+        if null sub
+          then return (BookApi {title = t, subject = sub, author_name = a})
+          else return (BookApi {title = t, subject = ["Other"], author_name = a})
 
 data SearchResponse = SearchResponse {docs :: [BookApi], num_found :: Int}
   deriving (Eq, Generic)
